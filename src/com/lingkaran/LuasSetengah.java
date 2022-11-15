957cb06008b89b25cea2a77d03a79d9075f5371a
Reza-shar <113996651+Reza-shar@users.noreply.github.com>
2022-11-15 21:02:27 +0700

    Update LuasSetengah.java

--- src/com/lingkaran/LuasSetengah.java
+++ src/com/lingkaran/LuasSetengah.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.lingkaran;
-
-/**
- *
- * @author SHIKUZIKA
- */
 public class LuasSetengah {
+      private Lingkaran lingkaran;
     
+    public LuasSetengah(Lingkaran lingkaran){
+        this.lingkaran = lingkaran;
+    }       
+    
+    public void hitung(){
+        this.lingkaran.hitungLuas();
+                
+    }
+    public void tampilkan(){
+        this.lingkaran.tampilkanluas();
+    }
 }
