94aab60ffc4d394d00a222b5003564b4022a5bf8
Reza-shar <113996651+Reza-shar@users.noreply.github.com>
2022-11-15 21:00:45 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author SHIKUZIKA
- */
+import com.lingkaran.*;
 public class Main {
-    
+     public static void main(String[] args) {
+        Lingkaran circle_satu  = new SetengahLingkaran();
+        LuasSetengah luassatu = new LuasSetengah(circle_satu);
+        
+       luassatu.hitung();
+       luassatu.tampilkan();
+        
+        
+    }
 }
