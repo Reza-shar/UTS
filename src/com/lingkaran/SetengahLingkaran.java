958af711c50fce78985b6b99e77b99d3a9f92e46
Reza-shar <113996651+Reza-shar@users.noreply.github.com>
2022-11-15 21:02:58 +0700

    Update SetengahLingkaran.java

--- src/com/lingkaran/SetengahLingkaran.java
+++ src/com/lingkaran/SetengahLingkaran.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.lingkaran;
+public class SetengahLingkaran implements Lingkaran {
+     double hasil;
+    public SetengahLingkaran(){
+        hasil = 0;
+    }
 
-/**
- *
- * @author SHIKUZIKA
- */
-public class SetengahLingkaran {
-    
+    @Override
+    public void hitungLuas() {
+        double luas1 = (PANJANG / 2 * LEBAR) - (Math.PI * (PANJANG / 2) * (PANJANG / 2) / 4);
+        hasil = 2 * luas1;
+    }
+
+    @Override
+    public void tampilkanluas() {
+        System.out.println("luasnya adalah :" + this.getHasil());
+    }
+    public double getHasil(){
+        return hasil;
+    }
+   
 }
